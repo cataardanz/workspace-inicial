08d2f0dbd137d023235df2ecdace158bcf8a6a61
aprilasmyname <sofiaavrilsaldivia@outlook.com>
2024-08-22 20:47:55 -0300

    redirigir pagina a login cuando no se este iniciada la sesion

--- js/index.js
+++ js/index.js
@@ -11,4 +11,8 @@ document.addEventListener("DOMContentLoaded", function(){
         localStorage.setItem("catID", 103);
         window.location = "products.html"
     });
+    
+    if (localStorage.getItem("loggedIn") !== "true") {
+        window.location.href = "login.html";
+    }
 });
